26ebd7bedd5b42928cd2ec8725e60cb5a922605e
simonlundmark1 <155955074+simonlundmark1@users.noreply.github.com>
2024-12-04 13:59:48 +0100

    lagt till en cool text

--- reseplanerare/client/src/main.tsx
+++ reseplanerare/client/src/main.tsx
@@ -6,5 +6,6 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <div id="modal-root"><p>JAG ÄR EN DATAHJÄRNA!</p></div>
   </StrictMode>,
 )
